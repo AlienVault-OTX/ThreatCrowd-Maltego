61d564a0090283071b37998ca212172df3d97fb0
threatcrowd <threatcrowd@gmail.com>
2015-07-26 19:20:09 +0100

    ThreatCrowd Maltego
    Maltego transforms for the ThreatCrowd API

---
+++ tc_md5.py
@@ -0,0 +1,54 @@
+#############################################
+# ThreatCrowd API v1.0 Domain Lookup
+# 
+# Author:	@chrisdoman
+# Email:	threatcrowd@gmail.com
+# Date:		2015-07-26
+#############################################
+import json
+import urllib2
+import sys
+from MaltegoTransform import *
+
+
+def main():
+
+
+	md5 = sys.argv[1]
+	
+	url = "https://www.threatcrowd.org/searchApi/v1/api.php?type=md5&query=" + md5
+	
+	response = urllib2.urlopen(url)
+	html = response.read()
+	
+
+	
+	for line in html.split('\r'):
+		if "," in line:
+			l = line.strip()
+			type = l.split(',')[0]
+			value = l.split(',')[1]
+			reference = l.split(',')[2]
+			
+			
+			if type == "DOMAIN":
+				m.addEntity("maltego.Domain", value)
+			if type == "IP":
+				m.addEntity("maltego.IPv4Address", value)
+			if type == "MD5":
+				m.addEntity("malformity.Hash", value)			
+			if type == "EMAIL":
+				m.addEntity("maltego.EmailAddress", value)						
+				
+		
+	return
+
+if __name__ == '__main__':
+	m = MaltegoTransform()
+	m.addUIMessage("[INFO] Enriching domain via ThreatCrowd")
+	try:
+		main()
+	except Exception as e:
+		m.addUIMessage("[Error] " + str(e))
+	m.returnOutput()
+	
